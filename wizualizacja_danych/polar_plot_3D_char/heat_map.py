800d973f60374ddeb1ff557db9fa77aef32bfe7b
Pablerinio <paollo.hat@gmail.com>
2024-05-06 17:33:24 +0200

    heatmapa

---
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_multiple_patterns_from_csv(file,patterns):
+    df = pd.read_csv(file, sep=';', header=None, names=['horizontal', 'vertical', 'patern', 'freq', 'power'])
+    for patern in patterns:
+        df_patern = df[df['patern'] == patern]
+        heatmap_data = df_patern.pivot_table(index='vertical', columns='horizontal', values='power')
+        plt.figure(figsize=(10, 7))
+        sns.heatmap(heatmap_data, cmap='viridis', fmt=".2g",)
+        plt.gca().invert_yaxis()
+        plt.title(f'Heatmapa mocy dla paternu {patern}')
+        plt.xlabel('Kąt horizontal')
+        plt.ylabel('Vertical')
+        plt.savefig(f'heatmapa_patern_{patern}.jpg', format='jpg')
+        plt.show()
+
+
+
+file_path=r'C:\Users\paoll\RIS\Python_RIS\wyniki\charakterystyka_3D\24_04_ch_ka_3D_5_5Ghz_1_5m.csv'
+patterns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+plot_multiple_patterns_from_csv(file_path, patterns)
--- wizualizacja_danych/polar_plot_3D_char/polar_plot.py
+++ wizualizacja_danych/polar_plot_3D_char/polar_plot.py
@@ -6,7 +6,6 @@ import numpy as np
 def plot_multiple_patterns_from_csv(file_path, patterns,vertical_val):
     df = pd.read_csv(file_path, delimiter=';', header=None)
     df.columns = ['horizontal', 'vertical', 'patern', 'freq', 'power']
-
     for patern in patterns:
         for vertical in vertical_val:
             # Wybieramy dane dla danego paterns i vertical
@@ -28,7 +27,7 @@ def plot_multiple_patterns_from_csv(file_path, patterns,vertical_val):
             # Pokazanie wykresu
             plt.show()
 
-file_path=r'C:\Users\Paweł\Desktop\Kliks\Ris\Python_RIS\wyniki\charakterystyka_3D\24_04_ch_ka_3D_5_5Ghz_1_5m.csv'
-patterns=[1,2,3]
-vertical=[-27,-18,-9]
+file_path=r'C:\Users\paoll\RIS\Python_RIS\wyniki\charakterystyka_3D\24_04_ch_ka_3D_5_5Ghz_1_5m.csv'
+patterns=[13,14,15,16,17]
+vertical=[-27,-18,-9,9,18,27]
 plot_multiple_patterns_from_csv(file_path, patterns,vertical)
