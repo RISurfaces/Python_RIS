9a787661784470a75623f9e00a3f39fd5b7b4dd3
Pablerinio <paollo.hat@gmail.com>
2024-05-07 09:28:10 +0200

    vmin max

--- wizualizacja_danych/polar_plot_3D_char/heat_map.py
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -9,7 +9,7 @@ def plot_multiple_patterns_from_csv(file,patterns):
         df_patern = df[df['patern'] == patern]
         heatmap_data = df_patern.pivot_table(index='vertical', columns='horizontal', values='power')
         plt.figure(figsize=(10, 7))
-        sns.heatmap(heatmap_data, cmap='viridis', fmt=".2g",)
+        sns.heatmap(heatmap_data,vmax=-50,vmin=-100, cmap='viridis', fmt=".2g",)
         plt.gca().invert_yaxis()
         plt.title(f'Heatmapa mocy dla paternu {patern}')
         plt.xlabel('Kąt horizontal')
@@ -20,6 +20,6 @@ def plot_multiple_patterns_from_csv(file,patterns):
 
 
 
-file_path=r'..\..\wyniki\charakterystyka_3D\24_04_ch_ka_3D_5_5Ghz_1_5m_oryginal.csv'
+file_path=r'..\..\wyniki\charakterystyka_3D\24_04_ch_ka_3D_5_5Ghz_1_5m_jest_zero.csv'
 patterns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 plot_multiple_patterns_from_csv(file_path, patterns)
