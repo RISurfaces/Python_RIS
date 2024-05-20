6afeea9a7fb6de28e49eb420440e8604b1881de2
Pablerinio <paollo.hat@gmail.com>
2024-05-20 21:32:20 +0200

    ,,,

--- wizualizacja_danych/polar_plot_3D_char/heat_map.py
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -7,18 +7,24 @@ def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
     df = pd.read_csv(file, sep=';', header=None, names=['vertical', 'horizontal', 'pattern', 'freq', 'power'])
     for pattern in patterns:
         df_pattern = df[(df['pattern'] == pattern) & (df['horizontal'] >= horizontal_range[0]) & (df['horizontal'] <= horizontal_range[1])]
-        heatmap_data = df_pattern.pivot_table(index='horizontal', columns='vertical', values='power')
+        heatmap_data = df_pattern.pivot_table(index='vertical', columns='horizontal', values='power')
         plt.figure(figsize=(10, 7))
-        sns.heatmap(heatmap_data, vmax=-50, vmin=-100, cmap='viridis', fmt=".2g")
+        #sns.set(font_scale=1.2)  # Set the font scale for seaborn heatmap
+        sns.heatmap(heatmap_data, vmax=-50, vmin=-100, cmap='viridis', annot=False, fmt=".2g")
         plt.gca().invert_yaxis()
-        plt.title(f'Power Heatmap for Pattern {pattern}')
-        plt.xlabel('Horizontal Angle')
-        plt.ylabel('Vertical Angle')
+        plt.title(f'Power Heatmap for Pattern {pattern}', fontsize=22)
+        plt.xlabel('Horizontal Angle', fontsize=18)
+        plt.ylabel('Vertical Angle', fontsize=18)
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+
+        
         plt.savefig(f'heatmap_pattern_{pattern}.jpg', format='jpg')
         plt.show()
 
 
 
-file_path=r'/Users/dawidbrzakala/Python_RIS/wyniki/charakterystyka_3D/09_05_ch_ka_3D_5_5Ghz_1_5m_blacha_miedz.csv'
-patterns=[-1]
+
+file_path=r'wyniki/charakterystyka_3D/24_04_ch_ka_3D_5_5Ghz_1_5m_jest_zero.csv'
+patterns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 plot_multiple_patterns_from_csv(file_path, patterns)
