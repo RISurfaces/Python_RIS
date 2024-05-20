e791b4e42005453c8c5b46d10f0b5c6f57a1de9d
Pablerinio <paollo.hat@gmail.com>
2024-05-20 20:37:51 +0200

    heatmap

--- wizualizacja_danych/polar_plot_3D_char/heat_map.py
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -3,20 +3,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_multiple_patterns_from_csv(file,patterns):
-    df = pd.read_csv(file, sep=';', header=None, names=['vertical', 'horizontal', 'patern', 'freq', 'power'])
-    for patern in patterns:
-        df_patern = df[df['patern'] == patern]
-        heatmap_data = df_patern.pivot_table(index='horizontal', columns='vertical', values='power')
+def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
+    df = pd.read_csv(file, sep=';', header=None, names=['vertical', 'horizontal', 'pattern', 'freq', 'power'])
+    for pattern in patterns:
+        df_pattern = df[(df['pattern'] == pattern) & (df['horizontal'] >= horizontal_range[0]) & (df['horizontal'] <= horizontal_range[1])]
+        heatmap_data = df_pattern.pivot_table(index='horizontal', columns='vertical', values='power')
         plt.figure(figsize=(10, 7))
-        sns.heatmap(heatmap_data,vmax=-50,vmin=-100, cmap='viridis', fmt=".2g",)
+        sns.heatmap(heatmap_data, vmax=-50, vmin=-100, cmap='viridis', fmt=".2g")
         plt.gca().invert_yaxis()
-        plt.title(f'Heatmapa mocy dla paternu {patern}')
-        plt.xlabel('Kąt horizontal')
-        plt.ylabel('Vertical')
-        plt.savefig(f'heatmapa_patern_{patern}.jpg', format='jpg')
+        plt.title(f'Power Heatmap for Pattern {pattern}')
+        plt.xlabel('Horizontal Angle')
+        plt.ylabel('Vertical Angle')
+        plt.savefig(f'heatmap_pattern_{pattern}.jpg', format='jpg')
         plt.show()
-
 
 
 
