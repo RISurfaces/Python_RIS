4925d294aee472d93e78a54c065e38a404f50e3f
DawidBrzaki <brza.ki00@gmail.com>
2024-06-15 12:09:21 +0200

    nadal heatmapy....

--- wizualizacja_danych/polar_plot_3D_char/heat_map.py
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
-    df = pd.read_csv(file, sep=';', header=None, names=['horizontal','vertical' , 'pattern', 'freq', 'power'])
+    df = pd.read_csv(file, sep=';', header=None, names=['vertical','horizontal' , 'pattern', 'freq', 'power'])
     for pattern in patterns:
         df_pattern = df[(df['pattern'] == pattern) & (df['horizontal'] >= horizontal_range[0]) & (df['horizontal'] <= horizontal_range[1])]
         heatmap_data = df_pattern.pivot_table(index='vertical', columns='horizontal', values='power')
@@ -15,7 +15,7 @@ def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
 
         cbar.ax.yaxis.set_tick_params(labelsize=14) #Font size of colorbar numbers
         cbar.ax.yaxis.label.set_size(18) #Font size of colorbar label cbar_kws
-        distance=1
+        distance=1.5
         ax.invert_yaxis()
         plt.title(f'Heatmap for pattern {pattern} on {distance}m', fontsize=22)
         plt.xlabel('Azimuth angle [°]', fontsize=18)
@@ -24,13 +24,13 @@ def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
         plt.yticks(fontsize=14)
 
         
-        plt.savefig(f'heatmap_pattern_{pattern}_dist_{distance}m.jpg', format='jpg')
+        plt.savefig(f'heatmap_pattern_{pattern}_dist_{distance}m.jpg', format='jpg', bbox_inches='tight')
         #plt.show()
         plt.close()
 
 
 
 
-file_path=r'/Users/dawidbrzakala/Python_RIS/wyniki_surowe_dane/charakterystyka_3D/char_pozioma_3D/13_06_ch_ka_3D_5_5Ghz_1m.csv'
+file_path=r'/Users/dawidbrzakala/Python_RIS/wyniki_surowe_dane/charakterystyka_3D/char_pozioma_3D/24_04_ch_ka_3D_5_5Ghz_1_5m_jest_zero.csv'
 patterns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 plot_multiple_patterns_from_csv(file_path, patterns)
