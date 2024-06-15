f0377ca3123b4dd56f9657d34f84336751df79fc
DawidBrzaki <brza.ki00@gmail.com>
2024-06-15 11:08:13 +0200

    heatmaps

--- wizualizacja_danych/patterny_artykuł/gen_kilku_patternow.py
+++ wizualizacja_danych/patterny_artykuł/gen_kilku_patternow.py
@@ -14,7 +14,7 @@ def display_images(paths):
                 axs[i, j].axis('off')
                 axs[i, j].set_title(f"Pattern {i * 2 + j + 1}")  # Dodanie podpisu
                 
-    legend_labels = {"ON": "green", "OFF": "#C0F6C7"}
+    legend_labels = {"ON": "blue", "OFF": "lightblue"}
     legend_handles = [plt.Rectangle((0, 0), 1, 1, color=color, edgecolor='black', linewidth=1) for color in legend_labels.values()]
     legend_texts = list(legend_labels.keys())
     
@@ -27,10 +27,10 @@ def display_images(paths):
 image_paths = [
     "1_pattern.png",
     "2_pattern.png",
-    "17_pattern.png",
-    "19_pattern.png",
-    "21_pattern.png",
-    "26_pattern.png"
+    "3_pattern.png",
+    "4_pattern.png",
+    "5_pattern.png",
+    "6_pattern.png"
 ]
 
 # Wyświetlanie zdjęć
