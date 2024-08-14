640e751220db0fedbef07612085a4950285a4d51
Pablerinio <paollo.hat@gmail.com>
2024-08-14 21:53:19 +0200

    2m

--- wizualizacja_danych/polar_plot_3D_char/heat_map.py
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -14,21 +14,21 @@ def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
         cbar= ax.collections[0].colorbar
         cbar.ax.yaxis.set_tick_params(labelsize=14) #Font size of colorbar numbers
         cbar.ax.yaxis.label.set_size(18) #Font size of colorbar label cbar_kws
-        distance=1.5
+        distance=2
         ax.invert_yaxis()
-        plt.title(f'Heatmap for pattern {pattern} on {distance}m', fontsize=22)
+        plt.title(f'Patern {pattern} on {distance}m - chamber', fontsize=22)
         #plt.title(f'Heatmap for pattern {pattern} on {distance}m', fontsize=22)
         plt.xlabel('Azimuth angle [°]', fontsize=18)
         plt.ylabel('Elevation angle [°]', fontsize=18)
         plt.xticks(fontsize=14)
         plt.yticks(fontsize=14)
-        plt.savefig(f'heatmap_pattern_{pattern}_dist_{distance}m.jpg', format='jpg', bbox_inches='tight')
+        plt.savefig(f'heatmap_pattern_{pattern}_dist_{distance}m_chamber.jpg', format='jpg', bbox_inches='tight')
         #plt.show()
         plt.close()
 
 
 
 
-file_path=r'/Users/dawidbrzakala/Python_RIS/wyniki_surowe_dane/charakterystyka_3D/char_pionowa_3D/11_06_vertical_confirmation_2m.csv'
+file_path=r'C:\Users\Paweł\Desktop\RIS\Ris\Python_RIS\wyniki_surowe_dane\charakterystyka_3D_komora_PIT\12_08_ch_ka_3D_horizontal_5_5Ghz_2m_komora.csv'
 patterns=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 plot_multiple_patterns_from_csv(file_path, patterns)
