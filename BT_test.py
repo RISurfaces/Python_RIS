811d12591f2980bd75c75b1742530db05e1daefc
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2024-11-10 14:34:16 +0100

    BT tests

--- BT_test.py
+++ BT_test.py
@@ -1,29 +1,31 @@
 import asyncio
-import RIS_usb_class
+#import RIS_usb_class
 from bleak import *
 
 #set key
-ris = RIS_usb_class.RIS_usb('COM7', 1)
-ris.read_EXT_voltage()
-ris.set_BT_key(12345)
+#ris = RIS_usb_class.RIS_usb('COM7', 1)
+#ris.read_EXT_voltage()
+#ris.set_BT_key(12345)
 
 #find devices
-async def run():
+import asyncio
+from bleak import BleakScanner
+
+async def main():
     devices = await BleakScanner.discover()
-    for d in devices:
-        print(d)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+    for device in devices:
+        print(device)
+    ble_address = "6C:3E:67:FC:CD:06"
+    async with BleakClient(ble_address) as client:
+        # we’ll do the read/write operations here
+        print("Connected to BLE device")
+        print(client.is_connected)   
+
+asyncio.run(main())
 
 #check characteristic
-MAC = '00:18:DA:31:63:CB'
-ris = RIS_usb_class.RIS_usb('COM7', 1)
-ris.set_BT_key('12345')
-async def connection():
-    client = BleakClient(MAC)
-    await client.connect()
-    My_services = await client.get_services()
-    for s in My_services:
-        print(s) # will print all the servies the device is sending
-        My_chars =  s.get_characteristic(s)
-        print(str(My_chars))
+
+#ris = RIS_usb_class.RIS_usb('COM7', 1)
+#ris.set_BT_key('12345')
+
+    
