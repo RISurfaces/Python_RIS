25e5eb60f51dd616202398f4d6b5ab0596b39d07
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2024-11-11 19:55:11 +0100

    fix

--- test/csv_rewrite.py
+++ test/csv_rewrite.py
@@ -4,12 +4,12 @@ import os
 
 
 import csv
-with open('24_10_3D_5_5Ghz_1_5m_new_ant_accurate.csv', mode ='r')as file:
+with open('Python_RIS/wyniki_surowe_dane/charakterystyka_3D_nowe_anteny_AINFO/24_10_3D_5_5Ghz_1_5m_new_ant_accurate.csv', mode ='r')as file:
   csvFile = csv.reader(file, delimiter=';')
   with open ('24_10_3D_5_5Ghz_1_5m_new_ant.csv', 'w', encoding='utf-8') as f:
         w = csv.writer(f, delimiter=';')
         for row in csvFile:
             print(row)
-            if row[1] in ['-27.0', '-18.0', '-9.0', '0', '9.0', '18.0', '27.0']:
+            if row[1] in ['-27.0', '-18.0', '-9.0', '0.0', '9.0', '18.0', '27.0']:
                 w.writerow(row)
   
