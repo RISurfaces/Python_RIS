57c29a0fa9c9d7ac67d1c92f0047e3c3d42c010b
DawidBrzaki <brza.ki00@gmail.com>
2024-11-13 20:36:59 +0100

    new plots

--- wizualizacja_danych/polar_plot_3D_char/heat_map.py
+++ wizualizacja_danych/polar_plot_3D_char/heat_map.py
@@ -11,11 +11,11 @@ def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
         heatmap_data = df_pattern.pivot_table(index='vertical', columns='horizontal', values='power')
         plt.figure(figsize=(11, 10))
         #sns.set(font_scale=1.2)  # Set the font scale for seaborn heatmap
-        ax= sns.heatmap(heatmap_data, vmax=-50, vmin=-100,cbar_kws={'label': 'Received power [dBm]'}, cmap='viridis', annot=False, fmt=".2g")
+        ax= sns.heatmap(heatmap_data, vmax=-30, vmin=-100,cbar_kws={'label': 'Received power [dBm]'}, cmap='viridis', annot=False, fmt=".2g")
         cbar= ax.collections[0].colorbar
         cbar.ax.yaxis.set_tick_params(labelsize=14) #Font size of colorbar numbers
         cbar.ax.yaxis.label.set_size(18) #Font size of colorbar label cbar_kws
-        distance=1.5
+        distance=1_5
         ax.invert_yaxis()
         plt.title(f'Patern {pattern} on {distance}m', fontsize=22)
         #plt.title(f'Heatmap for pattern {pattern} on {distance}m', fontsize=22)
@@ -30,6 +30,6 @@ def plot_multiple_patterns_from_csv(file, patterns, horizontal_range=(45, 135)):
 
 
 
-file_path=r'C:\Users\Marcel\Python_RIS\Python_RIS\wyniki_surowe_dane\charakterystyka_3D_nowe_anteny_AINFO\24_10_3D_5_5Ghz_1_5m_new_ant.csv'
+file_path=r'C:\Users\Brzaki\Documents\Python_RIS\wyniki_surowe_dane\charakterystyka_3D_nowe_anteny_AINFO\24_10_3D_5_5Ghz_1_5m_new_ant.csv'
 patterns=[1, 20, 17, 8]
 plot_multiple_patterns_from_csv(file_path, patterns)
