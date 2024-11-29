3b06dc9cf3074d5cdd19aae6569466e415877c62
DawidBrzaki <brza.ki00@gmail.com>
2024-11-29 12:15:35 +0100

    metrics file xlsx

--- metryki_porownywanie/metryki.py
+++ metryki_porownywanie/metryki.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
 from time import perf_counter
 
@@ -6,7 +6,19 @@ def read_values_from_file(file_path):
     data_file = pd.read_csv(file_path, sep=';', header=None, names=['horizontal','vertical' , 'pattern', 'freq', 'power'])
     
     return data_file
+
+def read_values_from_file_UNOREVERSE(file_path):
+    data_file = pd.read_csv(file_path, sep=';', header=None, names=['vertical','horizontal' , 'pattern', 'freq', 'power'])
     
+    return data_file
+ 
+def save_metrics_multiple_patterns_to_excel(filename, results):
+    # Tworzenie DataFrame z wynikami
+    df_metrics = pd.DataFrame(results, columns=['Pattern', 'MSE', 'MAE', 'SSD', 'SSD Znormalizowane', 'CC', 'CC Znormalizowane'])
+    
+    # Zapis do pliku Excel
+    df_metrics.to_excel(filename, index=False, sheet_name='Metrics')
+    print(f'Wyniki dla wielu patternów zostały zapisane do pliku {filename}')
 
 def data_filter(data_file_X, horizontal_range,pattern):
     # Filtracja danych dla obu plików
@@ -65,23 +77,52 @@ def MAE(data_X, data_Y):
 
     return result
 
+
+
 #MAIN
-path_X = '/Users/dawidbrzakala/Python_RIS/wyniki_surowe_dane/charakterystyka_3D/char_pozioma_3D/13_06_ch_ka_3D_5_5Ghz_1m.csv'
-path_Y = '/Users/dawidbrzakala/Python_RIS/wyniki_surowe_dane/charakterystyka_3D_nowe_anteny_AINFO/28_10_3D_5_5Ghz_1m_new_ant.csv'
+path_X = '/Users/dawidbrzakala/Python_RIS_nowy/Python_RIS/wyniki_surowe_dane/charakterystyka_3D/char_pozioma_3D/13_06_ch_ka_3D_5_5Ghz_1m.csv'
+path_Y = '/Users/dawidbrzakala/Python_RIS_nowy/Python_RIS/wyniki_surowe_dane/charakterystyka_3D_komora_PIT/13_08_3D_5_5Ghz_1m_chamber.csv'
 pattern = 18
 horizontal_range=(45, 135) #Oganiczenie pobierania danych - dane z azymutu od 45 do 135 stopni
 data_set_X, data_set_Y = read_values_from_file(path_X), read_values_from_file(path_Y)
 pattern_set_X,pattern_set_Y = data_filter(data_set_X,horizontal_range,pattern), data_filter(data_set_Y,horizontal_range,pattern)
 print('\n')
 
-wartosc = MSE(pattern_set_X,pattern_set_Y)
-print(f'Wynik MSE: {wartosc} \nIm mniejsza wartość MSE, tym obrazy są bardziej podobne.\n')
+# Lista do przechowywania wyników
+results = []
 
-wartosc = MAE(pattern_set_X,pattern_set_Y)
-print(f'Wynik MAE: {wartosc}')
+# Iteracja po patternach
+for pattern in range(1, 28):  # Od 1 do 27 włącznie
+    try:
+        # Filtrowanie danych dla bieżącego pattern
+        pattern_set_X = data_filter(data_set_X, horizontal_range, pattern)
+        pattern_set_Y = data_filter(data_set_Y, horizontal_range, pattern)
+        
+        # Obliczanie metryk
+        mse_value = MSE(pattern_set_X, pattern_set_Y)
+        mae_value = MAE(pattern_set_X, pattern_set_Y)
+        ssd_value, ssd_normalized = SSD(pattern_set_X, pattern_set_Y)
+        cc_value, cc_normalized = CC(pattern_set_X, pattern_set_Y)
+        
+        # Dodawanie wyników do listy
+        results.append([pattern, mse_value, mae_value, ssd_value, ssd_normalized, cc_value, cc_normalized])
+    
+    except Exception as e:
+        print(f'Błąd dla patternu {pattern}: {e}')
+        results.append([pattern, None, None, None, None, None, None])  # Dodaj puste wartości w przypadku błędu
 
-wartosc,wartosc_znormalizowana = SSD(pattern_set_X,pattern_set_Y)
-print(f'Wynik SSD: {wartosc}, Znormalizowane: {wartosc_znormalizowana}')
+# Zapis do pliku Excel
+output_filename = 'metryki_1m.xlsx'
+save_metrics_multiple_patterns_to_excel(output_filename, results)
 
-wartosc_CC,wartosc_znormalizowana_CC = CC(pattern_set_X,pattern_set_Y)
-print(f'Wynik CC: {wartosc_CC}, Znormalizowane: {wartosc_znormalizowana_CC}')
+# wartosc = MSE(pattern_set_X,pattern_set_Y)
+# print(f'Wynik MSE: {wartosc} \nIm mniejsza wartość MSE, tym obrazy są bardziej podobne.\n')
+
+# wartosc = MAE(pattern_set_X,pattern_set_Y)
+# print(f'Wynik MAE: {wartosc}')
+
+# wartosc,wartosc_znormalizowana = SSD(pattern_set_X,pattern_set_Y)
+# print(f'Wynik SSD: {wartosc}, Znormalizowane: {wartosc_znormalizowana}')
+
+# wartosc_CC,wartosc_znormalizowana_CC = CC(pattern_set_X,pattern_set_Y)
+# print(f'Wynik CC: {wartosc_CC}, Znormalizowane: {wartosc_znormalizowana_CC}')
