b2e7a815527a16b55c6c1c3a0d577a3cd3d5fbae
mgarczyk <madzelsky@gmail.com>
2025-01-27 15:49:39 +0100

    ff

--- MQTT_pattern_change.py
+++ MQTT_pattern_change.py
@@ -53,6 +53,7 @@ def check_RIS_metadata(RIS_list: list, client: mqtt_client):
 def set_pattern_with_ack(RIS_list: list, client: mqtt_client, val: str):
     for ris in RIS_list:
         ris.set_pattern(val)
+        time.sleep(0.1)
         pattern = ris.read_pattern()
         if pattern != None:
             val = pattern[3:-1]
