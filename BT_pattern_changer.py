fcbb391214c0aeb32dfd5d2cd11ff7f631b23d9b
mgarczyk <madzelsky@gmail.com>
2025-07-14 11:59:54 +0200

    bt tests

--- BT_pattern_changer.py
+++ BT_pattern_changer.py
@@ -40,7 +40,8 @@ try:
 except FileNotFoundError:
     print("File with patterns doesn't exist.")
     exit()
-
-ris_ble = RIS_ble.RIS_ble("A-32388F", 0)
-response = asyncio.run(ris_ble.connect_multiple_patterns(patterns_data))
-print(response)
+while True:
+    input("Potwierdz start pomiaru: ")
+    ris_ble = RIS_ble.RIS_ble("A-3163CA", 0)
+    response = asyncio.run(ris_ble.connect_multiple_patterns())
+    print(response)
